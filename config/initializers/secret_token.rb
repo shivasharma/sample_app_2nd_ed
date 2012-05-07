09cc7539c059eb7d69d1a7af470b36ee9f20cdbc
Shiva Sharma <ss12@umbc.edu>
2012-05-06 21:26:02 -0400

    Ruby 3.1.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp31::Application.config.secret_token = 'ddaf232c85139e07a710823c4c7fad81f85868e29b4101a3e284b97d6ad1a1e8fe519936ba74734f6270a6e67465204af64400000e67bba2093069f2c3c3e4fb'
